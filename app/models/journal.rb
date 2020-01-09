da91ccb6ec8a622d2a3b6de1cd0c284df9557a12
Emily <emily.a.harber@gmail.com>
2020-01-09 13:49:14 -0500

    add some validations to user and journal

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,4 +2,8 @@ class Journal < ApplicationRecord
   belongs_to :user
   has_many :entries 
   has_many :moods, through: :entries 
+
+  validates :user_id, presence: true 
+  validates :title, presence: true, length: { maximum: 20, message: "Title must be less than 20 characters long." }
+  
 end
