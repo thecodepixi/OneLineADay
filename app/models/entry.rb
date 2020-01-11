2dc776e858b07ba81e85f11e2d8fca5068909920
Emily <emily.a.harber@gmail.com>
2020-01-10 19:04:39 -0500

    add username validation to User, add content length validation to Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,6 @@ class Entry < ApplicationRecord
   belongs_to :user 
   belongs_to :mood 
   belongs_to :journal 
+
+  validates :content, length: { maximum: 150, message: "Journal entries can only be up to 150 characters." }
 end
