cf5c29b0cdeb1a64921749772203f251bd957b71
Emily <emily.a.harber@gmail.com>
2020-01-11 14:56:57 -0500

    Create User actions skeleton, and create files for needed User views.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,23 @@ class UserController < ApplicationController
 
   def login
   end 
+
+  def logout
+  end 
+
+  def new
+  end 
   
+  def create 
+  end 
+
+  def edit
+  end 
+  
+  def update
+  end 
+
+  def delete
+  end 
+
 end
