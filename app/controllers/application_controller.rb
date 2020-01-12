c31a6f1c304347a10ce800727c22c1d99646e121
Emily <emily.a.harber@gmail.com>
2020-01-12 12:40:32 -0500

    Add Bootstrap for styling. Start work on UserController actions and Application helper mthods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  #create session on login 
+  def set_session(user)
+    session[:user_id] = user.id 
+  end 
+
+  #persist current logged in User object
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
+  end 
+
+  
+
 end
