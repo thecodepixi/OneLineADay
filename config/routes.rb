a0b6a210a88d34c48e896e9edf9674a030c8704f
Emily <emily.a.harber@gmail.com>
2020-01-14 17:15:11 -0500

    add comment with desired routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,42 @@ Rails.application.routes.draw do
   post '/logout', controller: 'users', action: 'logout'
   
   resources :users do 
-    resources :journals do
-      resources :days, only: :index
-    end 
+    resources :journals, only: :index 
   end 
 
-  resources :days, except: :index
+  resources :journals, except: :index do
+    resources :days 
+  end
 
 end
+
+# Routes I Want: 
+# USERS
+# get / => users#login or users#show
+# get /users/:id => users#show
+# delete /users/:id => users#destroy
+# get /users/:id/edit => users#edit
+# put/patch /users/:id => users#update 
+# get /users/new => users#new
+# post /users => users#create
+# USERS JOURNALS
+# get /users/:user_id/journals => journals#index 
+# get /users/:user_id/journals/new => journal#new 
+# post /users/:user_id/journals => journal#create 
+# get /users/:user_id/journals/:id => journals#show
+# USERS MOODS
+# get /users/moods => moods#index 
+# get /users/moods/:id => moods#show 
+# JOURNALS
+# get /journals/:id/edit => journal#edit
+# put/patch /journals/:id => journal#update 
+# delete /journals/:id => journals#destroy 
+# JOURNALS DAYS
+# get /journals/:id/days => days#index
+# get /journals/:journal_id/days/:id => days#show 
+# get /journals/:journal_id/days/new => days#new 
+# post /journals/:journal_id/days => days#create 
+# DAYS
+# get days/:id/edit => days#edit 
+# put/patch days/:id => days#update 
+# delete days/:id => days#destroy 
