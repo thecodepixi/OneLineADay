ddb698c6ad2b4a1ea39409fcd105f902aa53a21e
Emily <emily.a.harber@gmail.com>
2020-01-14 20:30:44 -0500

    lots of DaysController and Days views work. New form errors not working.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+  gem 'pry'
 end
 
 group :test do
--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,17 +1,33 @@
 class DaysController < ApplicationController
 
   def index 
-    @user = User.find(params[:user_id])
-    @journal = @user.journals.find(params[:journal_id])
+    @journal = Journal.find(params[:journal_id])
   end 
 
   def show 
-    @day = Day.find_by(params[:id])
+    @day = Day.find_by(id: params[:id])
     @user = @day.user 
   end 
 
   def new 
-    
+    @journal = Journal.find(params[:journal_id])
+    @user = @journal.user 
+    @day = @journal.days.build
+  end 
+
+  def create
+    @day = Day.new(day_params)
+    if @day.save 
+      redirect_to days_path(@day)
+    else 
+      render 'days/new'
+    end 
+  end 
+
+  private 
+
+  def day_params 
+    params.require(:day).permit(:journal_id, :user_id, :mood_id, :content)
   end 
 
 end
